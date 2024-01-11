f83732aa418374954bb812088df9c61d753eccb1
reptic <70032959+repticJS@users.noreply.github.com>
2024-01-11 23:24:02 +0000

    Add files via upload

---
+++ Models/Company.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = new mongoose.Schema({
+
+    ID: {
+        type: String
+    },
+    Name: {
+        type: String
+    },
+    Logo: {
+        type: String
+    },
+    Description: {
+        type: String
+    },
+    Users: {
+        type: Array
+    },
+    Clients: {
+        type: Array
+    }
+
+})
+
+const model = new mongoose.model('Company', Schema);
+
+module.exports = model;
